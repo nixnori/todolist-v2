3d0f8f2237202f6e8d01e6ed63afbbff32aa3bb3
nixnori <nikkinoriega@outlook.com>
2020-11-02 16:25:39 -0500

    Connect todo list app to mondoDB via mongoose

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-//jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,37 +11,77 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true})
+
+const itemSchema = new mongoose.Schema ({
+  name: String
+})
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item ({
+  name: "This is item 1."
+})
+
+const item2 = new Item ({
+  name: "This is item 2."
+})
+
+const item3 = new Item ({
+  name: "This is item 3."
+})
+
+const defaultItems = [item1, item2, item3]
 
 app.get("/", function(req, res) {
+  
+  Item.find({}, (err, foundItems) => {
+    
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, err => {
+        if (err) {
+          console.log(err) 
+        } else {
+          console.log("Item added successfully!")
+        }
+      })
+      
+      res.redirect("/")
+    } else {
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
 
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
+  })
 
 });
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item ({
+    name: itemName
+  })
+
+  item.save()
+
+  res.redirect("/")
+  
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
+app.post("/delete", (req, res) => {
+  const checkedItemId = req.body.checkbox;
+  console.log(checkedItemId)
 
-app.get("/about", function(req, res){
-  res.render("about");
-});
+  Item.findOneAndDelete({_id: checkedItemId}, function(err) {
+    if (!err) {
+      console.log("Delete successful.")
+      res.redirect("/")
+    }
+  })
+
+})
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
